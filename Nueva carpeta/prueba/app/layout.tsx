21837be71eedfc4566883ffe3f456bed7960c807
gaston <ztonchyarg@hotmail.com>
2023-01-06 17:33:46 -0300

    Nuevo Repo

---
+++ Nueva carpeta/prueba/app/layout.tsx
@@ -0,0 +1,22 @@
+import Navegation from './componentes/navegation';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import SearchAppBar from './componentes/NavBar/navBar'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <UserProvider>
+    <html lang="en">
+      <head />
+      <body>
+       <SearchAppBar/> 
+      <Navegation />
+        {children}
+        </body>
+    </html>
+    </UserProvider>
+  )
+}
